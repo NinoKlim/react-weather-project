eb0255f8838e70c0ac8749aab8b5524ba2f8a282
NinaKlim <nnikitka@gmail.com>
2023-04-17 16:34:35 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container-fluid bgContainer">
+      <div className="container-fluid bgContainer mb-3">
         <Weather />
-        <footer>
-          <a href="/" target="_blank" rel="noreferrer"></a>
-        </footer>
       </div>
+      <footer>
+        Coded by Nina Klymenko,{" "}
+        <a
+          href="https://github.com/NinoKlim/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
